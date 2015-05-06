26d5c392e9828eb8562d958e9723a0b58c159c28
Kalimaha <guido.barbaglia@gmail.com>
2015-05-06 17:38:38 +0200

    Populate agricultural soils tables.

--- js/faostat-ui-analysis-ghg-qaqc.js
+++ js/faostat-ui-analysis-ghg-qaqc.js
@@ -158,30 +158,8 @@ define(['jquery',
 
         /* Chosen listener. */
         table_selector.change(function() {
-            _this.table_selector(domain_code, this.value);
+            _this.load_data($('#geographic_areas').val(), _this.get_selected_domain());
         });
-
-    };
-
-    GHG_QA_QC.prototype.table_selector = function(domain_code, table_type) {
-
-        /* Query DB, if needed. */
-        _this.query_db(this.value);
-
-        /* Load template. */
-        var source = $(templates).filter('#tables_' + table_type).html();
-        var template = Handlebars.compile(source);
-        var dynamic_data = {
-            id: domain_code,
-            nc_label: translate.nc,
-            co2eq_label: translate.co2eq,
-            faostat_label: translate.faostat,
-            difference_label: translate.difference,
-            export_data_label: translate.export_data_label,
-            norm_difference_label: translate.norm_difference
-        };
-        var html = template(dynamic_data);
-        $('#' + domain_code + '_tables_content').empty().html(html);
 
     };
 
@@ -211,36 +189,148 @@ define(['jquery',
         $('#geographic_areas').change(function() {
 
             /* Query DB, if needed. */
-            _this.query_db(this.value);
+            _this.load_data($('#geographic_areas').val(), _this.get_selected_domain());
 
         });
 
     };
 
-    GHG_QA_QC.prototype.query_db = function(area_code) {
+    GHG_QA_QC.prototype.load_data = function(area_code, domain_code) {
 
         /* This... */
         var _this = this;
-
-        console.log(area_code);
 
         /* Query DB, if needed. */
         if (this.CONFIG.data[area_code] == null) {
 
             /* SQL Query. */
-            var sql = 'SELECT DomainCode, Year, GUNFValue, GValue, PerDiff, NormPerDiff, TableType ' +
-                      'FROM DataUNFCCC ' +
-                      'WHERE AreaCode = \'' + area_code + '\' ';
+            var sql = _this.get_query(area_code);
 
             /* Query the DB. */
             Commons.wdstable(sql, function (json) {
 
-                /* Do something with the output. */
+                /* Store data. */
                 _this.CONFIG.data[area_code] = json;
+
+                /* Render tables. */
+                _this.render_tables(domain_code);
 
             }, 'http://localhost:8080/wds/rest', {datasource: this.CONFIG.datasource});
 
+        } else {
+
+            /* Render tables. */
+            this.render_tables(domain_code);
+
         }
+
+    };
+
+    GHG_QA_QC.prototype.render_tables = function(domain_code) {
+
+        /* Table type. */
+        var table_type = $('#' + domain_code + '_table_selector').val();
+
+        try {
+
+            /* Load template. */
+            var source = $(templates).filter('#tables_' + table_type).html();
+            var template = Handlebars.compile(source);
+            var dynamic_data = {
+                id: domain_code,
+                nc_label: translate.nc,
+                co2eq_label: translate.co2eq,
+                faostat_label: translate.faostat,
+                difference_label: translate.difference,
+                export_data_label: translate.export_data_label,
+                norm_difference_label: translate.norm_difference
+            };
+            var html = template(dynamic_data);
+            $('#' + domain_code + '_tables_content').empty().html(html);
+
+            /* Populate tables. */
+            this.populate_tables($('#geographic_areas').val());
+
+        } catch (e) {
+
+        }
+
+    };
+
+    GHG_QA_QC.prototype.get_query = function(area_code) {
+        return  'SELECT DomainCode, Year, UNFCCCCode, GUNFItemNameE, ' +
+                'GUNFValue, GValue, PerDiff, ' +
+                'NormPerDiff, TableType ' +
+                'FROM DataUNFCCC ' +
+                'WHERE AreaCode = \'' + area_code + '\' ' +
+                'ORDER BY UNFCCCCode, Year DESC';
+    };
+
+    GHG_QA_QC.prototype.get_selected_domain = function() {
+        return $($('#domains_tab').find('ul').find('li.active').find('a')[0]).attr('aria-controls');
+    };
+
+    GHG_QA_QC.prototype.populate_tables = function(area_code) {
+
+        /* Table type. */
+        var table_type = $('#gas_table_selector').val();
+
+        /* Populate GAS tables. */
+        this.populate_gas(area_code, table_type);
+
+    };
+
+    GHG_QA_QC.prototype.populate_gas = function(area_code, table_type) {
+
+        /* Common configuration. */
+        var wt_config = {
+            show_row_code: true,
+            row_code: 'UNFCCCCode',
+            lang: this.CONFIG.lang,
+            cols_dimension: 'Year',
+            row_label: 'GUNFItemNameE'
+        };
+
+        /* Data for tables. */
+        var gas_table_1 = [];
+        for (var i = 0 ; i < this.CONFIG.data[area_code].length ; i++) {
+            if (this.CONFIG.data[area_code][i].DomainCode == 'GAS' && this.CONFIG.data[area_code][i].TableType == table_type)
+                gas_table_1.push(this.CONFIG.data[area_code][i]);
+        }
+
+        /* Initiate wide tables library. */
+        var wt_1 = new WIDE_TABLES();
+        var wt_2 = new WIDE_TABLES();
+        var wt_3 = new WIDE_TABLES();
+        var wt_4 = new WIDE_TABLES();
+
+        /* Configure tables. */
+        var wt_1_config = $.extend(true, {}, wt_config, {
+            data: gas_table_1,
+            value_dimension: 'GValue',
+            placeholder_id: 'gas_table_1'
+        });
+        var wt_2_config = $.extend(true, {}, wt_config, {
+            data: gas_table_1,
+            value_dimension: 'GUNFValue',
+            placeholder_id: 'gas_table_2'
+        });
+        var wt_3_config = $.extend(true, {}, wt_config, {
+            data: gas_table_1,
+            value_dimension: 'PerDiff',
+            placeholder_id: 'gas_table_3'
+        });
+        var wt_4_config = $.extend(true, {}, wt_config, {
+            data: gas_table_1,
+            value_dimension: 'NormPerDiff',
+            placeholder_id: 'gas_table_4'
+        });
+
+        /* Render tables. */
+        wt_1.init(wt_1_config);
+        wt_2.init(wt_2_config);
+        wt_3.init(wt_3_config);
+        wt_4.init(wt_4_config);
 
     };
 
